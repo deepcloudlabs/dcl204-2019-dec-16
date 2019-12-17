0f3a4241e56ef891e4498a59fce859d035aaac49
binnur.kurt <binnur.kurt@gmail.com>
2019-12-17 16:43:35 +0300

    Study stream api and filter-map-reduce on Animals Application.

--- animals/src/com/example/animals/app/AnimalApp.java
+++ animals/src/com/example/animals/app/AnimalApp.java
@@ -2,6 +2,7 @@ package com.example.animals.app;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.ToIntFunction;
 
 import com.example.animals.domain.Animal;
 import com.example.animals.domain.Cat;
@@ -32,6 +33,21 @@ public class AnimalApp {
 			}
 		}
 		
+		int legs = 0;
+		for (Animal animal : animals)
+			if (animal instanceof Pet) 
+			   legs += animal.getLegs();
+		System.out.println(legs);
+		
+		ToIntFunction<Animal> mapToLegs = 
+//			animal -> animal.getLegs()	;
+				Animal::getLegs;
+		legs = animals.stream()
+//		       .filter(animal -> animal instanceof Pet)
+		       .filter(Pet.class::isInstance)
+		       .mapToInt(mapToLegs)
+		       .sum();
+		System.out.println(legs);
 	}
 
 }
